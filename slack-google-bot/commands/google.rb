5ab4f60556c6043b8f32633bb105d01b523dd286
Minh Danh <minhdanh@hoiio.com>
2016-06-22 17:57:17 +0700

    Fix no results returned

--- slack-google-bot/commands/google.rb
+++ slack-google-bot/commands/google.rb
@@ -3,13 +3,16 @@ module SlackGoogleBot
     class Google < SlackRubyBot::Commands::Base
       match(/^(?<bot>\w*)\s(?<expression>.*)$/) do |client, data, match|
         expression = match['expression'].strip
-        query = { query: expression }
-        query[:cx] = ENV['GOOGLE_CSE_ID'] if ENV['GOOGLE_CSE_ID']
-        result = ::Google::Search::Web.new(query).first
+        results = JSON.parse HTTP.get("https://www.googleapis.com/customsearch/v1", params: {
+              q: expression,
+              key: ENV['GOOGLE_API_KEY'],
+              cx: ENV['GOOGLE_CSE_ID']
+            })
+        result = results['items'].first
         if result.nil?
           message = "No search results for `#{expression}`"
         else
-          message = result.title + "\n" + result.uri
+          message = result['title'] + "\n" + result['link'] + "\n" + "```" + result['snippet'] + "```"
         end
         send_message client, data.channel, message
       end
