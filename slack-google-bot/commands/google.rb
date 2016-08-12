5ea0ef8a4f9aedbd4b8d88fcf30128eaa574e5c9
Minh Danh <minhdanh.ngo@gmail.com>
2016-08-12 22:00:36 +0700

    Comply with Rubocop

--- slack-google-bot/commands/google.rb
+++ slack-google-bot/commands/google.rb
@@ -5,12 +5,13 @@ module SlackGoogleBot
     class Google < SlackRubyBot::Commands::Base
       match(/^(?<bot>\w*)\s(?<expression>.*)$/) do |client, data, match|
         expression = match['expression'].strip
-        results = JSON.parse HTTP.get("https://www.googleapis.com/customsearch/v1", params: {
-              q: expression,
-              key: ENV['GOOGLE_API_KEY'],
-              cx: ENV['GOOGLE_CSE_ID']
-            })
-        result = results['items'].first
+        results = JSON.parse HTTP.get('https://www.googleapis.com/customsearch/v1',
+                                      params: {
+                                        q: expression,
+                                        key: ENV['GOOGLE_API_KEY'],
+                                        cx: ENV['GOOGLE_CSE_ID']
+                                      })
+        result = results['items'].nil? ? nil : results['items'].first
         if result.nil?
           message = "No search results for `#{expression}`"
         else
